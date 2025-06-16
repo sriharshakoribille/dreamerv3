f59f31c298077844bee171d76632740e891e6498
Sri Harsha Koribille <koribils@cs.uni-freiburg.de>
2025-06-16 14:23:39 +0200

    Gymnasium envs

--- dreamerv3/main.py
+++ dreamerv3/main.py
@@ -216,6 +216,7 @@ def make_env(config, index, **overrides):
     import memory_maze  # noqa
   ctor = {
       'dummy': 'embodied.envs.dummy:Dummy',
+      'gymnasium': 'embodied.envs.from_gymnasium:GymnasiumEnv',
       'gym': 'embodied.envs.from_gym:FromGym',
       'dm': 'embodied.envs.from_dmenv:FromDM',
       'crafter': 'embodied.envs.crafter:Crafter',
